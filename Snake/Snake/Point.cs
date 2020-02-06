85a10f6984b72319d373aafe088559da477c8840
P. Andrej <andrej.cdo@gmail.com>
2020-02-06 13:02:17 +0200

    Create a point class and use it to draw a point by coordinates

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Snake
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y); // Cursor position
+            Console.Write(sym); // draw symbol
+        }
+    }
+}
