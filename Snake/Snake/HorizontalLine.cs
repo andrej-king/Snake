abf550d5b6ac3fc358e869074f1217f1b9facd81
P. Andrej <andrej.cdo@gmail.com>
2020-02-06 15:24:03 +0200

    Create classes for draw vertical and horizontal lines

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Snake.Properties
+{
+    public class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            // List points
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        // Draw points with foreach
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Snake.Properties;
 
 namespace Snake
 {
@@ -8,50 +9,23 @@ namespace Snake
         public static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            // p1.Draw(); // draw point
 
             Point p2 = new Point(4, 5, '#');
-            // p2.Draw();
 
             Point p3 = new Point(3, 4, '^');
-            // p3.Draw();
-            
+
             Point p4 = new Point(12, 2, '+');
-            // p4.Draw();
-            
 
             Console.WriteLine();
 
-            // Create list
-            // List<int> numList = new List<int>();
-            // numList.Add(1);
-            // numList.Add(2);
-            // numList.Add(3);
-            //
-            // foreach (int i in numList)
-            // {
-            //     Console.WriteLine(i);
-            // }
-
-            // remove item with index 0
-            // numList.RemoveAt(0);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
+            
+            VerticalLine vLine = new VerticalLine( 3, 7, 20,'*');
+            vLine.Draw();
 
 
-            // List points
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-            
-            // Draw points with foreach
-            foreach (var VARIABLE in pList)
-            {
-                VARIABLE.Draw();
-            }
-            
-            
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
