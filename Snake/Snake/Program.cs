6bdbebcc3ed1a8c76a38e8f3a35a5d964b59f52c
P. Andrej <andrej.cdo@gmail.com>
2020-02-06 14:46:15 +0200

    Use List, draw points with foreach in List Points

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -7,11 +8,49 @@ namespace Snake
         public static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw(); // draw point
+            // p1.Draw(); // draw point
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            // p2.Draw();
 
+            Point p3 = new Point(3, 4, '^');
+            // p3.Draw();
+            
+            Point p4 = new Point(12, 2, '+');
+            // p4.Draw();
+            
+
+            Console.WriteLine();
+
+            // Create list
+            // List<int> numList = new List<int>();
+            // numList.Add(1);
+            // numList.Add(2);
+            // numList.Add(3);
+            //
+            // foreach (int i in numList)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // remove item with index 0
+            // numList.RemoveAt(0);
+
+
+            // List points
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+            
+            // Draw points with foreach
+            foreach (var VARIABLE in pList)
+            {
+                VARIABLE.Draw();
+            }
+            
+            
             Console.ReadKey();
         }
     }
