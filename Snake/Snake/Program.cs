9ad6ce924d5012f8cc47249ed3b112426316186c
P. Andrej <andrej.cdo@gmail.com>
2020-02-07 02:18:42 +0200

    Create food

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,42 +10,53 @@ namespace Snake
     {
         public static void Main(string[] args)
         {
+            Console.SetBufferSize(80, 25); // Set width and height window
 
-            Console.SetBufferSize(80, 25);  // Set width and height window
-            
             // Draw box
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine( 0, 24, 0,'+');
-            VerticalLine rightLine = new VerticalLine( 0, 24, 78,'+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
             upLine.Draw();
             downLine.Draw();
             leftLine.Draw();
             rightLine.Draw();
 
-            
+
             // draw points
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
 
             while (true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandeKey(key.Key);
                 }
-                Thread.Sleep(100);
-                snake.Move();
-                    
             }
-            
-            
+
 
             // Console.ReadKey();
-
         }
     }
 }
